1e04b4a80378908d782f99f9a956903cec7bc5f8
Ruben Baeza <ruben.baeza@santiago.mx>
2020-03-24 16:38:17 -0600

    clear inspect

--- lib/live_view_counter_web/views/page_view.ex
+++ lib/live_view_counter_web/views/page_view.ex
@@ -9,17 +9,14 @@ defmodule LiveViewCounterWeb.PageLiveView do
   end
 
   def mount(_params, _session, socket) do
-    IO.inspect("testerrr")
     {:ok, assign(socket, val: 0)}
   end
 
   def handle_event("inc", _value, socket) do
-    IO.inspect("testerrr")
     {:noreply, assign(socket, val: socket.assigns.val + 1 )}
   end
 
   def handle_event("dec", _value, socket) do
-    IO.inspect("testerrr dec")
     {:noreply, assign(socket, val: socket.assigns.val - 1 )}
   end
 
